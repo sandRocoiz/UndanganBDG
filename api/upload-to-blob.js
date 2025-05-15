0fa73a84708dbad6a6ac2d0c9f97f24cd954316b
Sandro Dwicahyo <sandrodwicahyo@gmail.com>
2025-05-15 11:47:59 +0700

    add vercelll.json

--- api/upload-to-blob.js
+++ api/upload-to-blob.js
@@ -1,9 +1,5 @@
 import { put } from "@vercel/blob";
 
-export const config = {
-  runtime: 'nodejs18.x',
-};
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
